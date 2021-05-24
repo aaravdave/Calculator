8f8a2fea2091a5342ff865dad67876e4b9320233
Aarav Dave <aarinaarav@gmail.com>
2021-05-24 13:35:27 -0400

    Create themes.py

---
+++ themes.py
@@ -0,0 +1,27 @@
+"""
+themes.py
+create hues for your calculator
+
+hexadecimal colors:  #HEXCODE
+base colors:         COLOR NAME
+grays:               grayNUM
+
+format:
+[background, foreground]
+
+submit themes:
+Email                    GitHub
+aarinaarav@gmail.com     aaravdave
+"""
+
+default = [                 # System
+    ['white', 'black'],     # Regular
+    ['#efad04', 'white'],   # Control
+    ['gray25', 'black']     # Operation
+]
+
+ocean = [                   # System
+    ['white', 'black'],     # Regular
+    ['#04c0ef', 'white'],   # Control
+    ['#f9df86', 'black']    # Operation
+]
